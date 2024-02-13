fc4384785301c7af8eefce8bd9942d76061ec50b
gdoggg2032 <gdoggg2032@gmail.com>
2024-02-13 12:11:49 +0800

    Redfield C:N:P ratio, more K, migrate to toml file format

--- planner.py
+++ planner.py
@@ -1,32 +1,44 @@
 
+
 import fire
+import tomlkit
 
 from data_type import Plan
-from util import optimize_containers, get_effects
+from util import get_effects, optimize_containers
 
 
 class Planner:
 
     def optimize_containers(self,
-                            input_path: str | None = './sample_plan.json',
-                            output_path: str | None = 'solved_plan.json',
+                            input_path: str | None = './sample_plan.toml',
+                            output_path: str | None = 'solved_plan.toml',
                             container_names: list[str] | None = None):
         print(f'container names: {container_names}')
-        plan = Plan.parse_file(input_path)
+        with open(input_path, 'r') as file_obj:
+            raw_dict = tomlkit.load(file_obj) # for preserve key order
+            plan = Plan.parse_obj(raw_dict)
         solved_plan = optimize_containers(plan,
                                           container_names=container_names,)
         with open(output_path, 'w') as file_obj:
-            file_obj.write(solved_plan.json(indent=4))
+            output_dict = raw_dict
+            output_dict.update(solved_plan.dict())
+            tomlkit.dump(output_dict, file_obj)
+
 
     def compute_effects(self,
-                        input_path: str | None = './sample_plan.json',
-                        output_path: str | None = 'solved_plan.json',
+                        input_path: str | None = './sample_plan.toml',
+                        output_path: str | None = 'solved_plan.toml',
                         ):
-        plan = Plan.parse_file(input_path)
+        with open(input_path, 'r') as file_obj:
+            raw_dict = tomlkit.load(file_obj) # for preserve key order
+            plan = Plan.parse_obj(raw_dict)
         # calculate effects
         effects = get_effects(plan)
         solved_plan = plan.copy(update=dict(effects=effects))
         with open(output_path, 'w') as file_obj:
-            file_obj.write(solved_plan.json(indent=4))  
+            output_dict = raw_dict
+            output_dict.update(solved_plan.dict())
+            tomlkit.dump(solved_plan.dict(), file_obj)
+
 if __name__ == '__main__':
     fire.Fire(Planner)
