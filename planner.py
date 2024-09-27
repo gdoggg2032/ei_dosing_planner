d5e13efa78e99950d8110ec786da0fccd82dad7e
gdoggg2032 <gdoggg2032@gmail.com>
2024-09-27 21:41:02 +0800

    remove GH Booster

--- planner.py
+++ planner.py
@@ -1,8 +1,10 @@
 
 
 import fire
-import tomlkit
+import tomllib
+import tomli_w
 
+from collections import OrderedDict
 from data_type import Plan
 from util import get_effects, optimize_containers
 
@@ -14,31 +16,30 @@ class Planner:
                             output_path: str | None = 'solved_plan.toml',
                             container_names: list[str] | None = None):
         print(f'container names: {container_names}')
-        with open(input_path, 'r') as file_obj:
-            raw_dict = tomlkit.load(file_obj) # for preserve key order
-            plan = Plan.parse_obj(raw_dict)
+        with open(input_path, 'rb') as file_obj:
+            plan = Plan.model_validate(tomllib.load(file_obj))
         solved_plan = optimize_containers(plan,
                                           container_names=container_names,)
-        with open(output_path, 'w') as file_obj:
-            output_dict = raw_dict
-            output_dict.update(solved_plan.dict())
-            tomlkit.dump(output_dict, file_obj)
+        with open(output_path, 'wb') as file_obj:
+            output_dict = solved_plan.model_dump()
+            ordered_dict = OrderedDict((field, output_dict.get(field)) for field in solved_plan.model_fields)
+            tomli_w.dump(ordered_dict, file_obj, multiline_strings=True)
 
 
     def compute_effects(self,
                         input_path: str | None = './sample_plan.toml',
                         output_path: str | None = 'solved_plan.toml',
                         ):
-        with open(input_path, 'r') as file_obj:
-            raw_dict = tomlkit.load(file_obj) # for preserve key order
-            plan = Plan.parse_obj(raw_dict)
+        with open(input_path, 'rb') as file_obj:
+            # raw_dict = tomllib.load(file_obj) # for preserve key order
+            plan = Plan.model_validate(tomllib.load(file_obj))
         # calculate effects
         effects = get_effects(plan)
-        solved_plan = plan.copy(update=dict(effects=effects))
-        with open(output_path, 'w') as file_obj:
-            output_dict = raw_dict
-            output_dict.update(solved_plan.dict())
-            tomlkit.dump(solved_plan.dict(), file_obj)
+        solved_plan = plan.model_copy(update=dict(effects=effects))
+        with open(output_path, 'wb') as file_obj:
+            output_dict = solved_plan.model_dump()
+            ordered_dict = OrderedDict((field, output_dict.get(field)) for field in solved_plan.model_fields)
+            tomli_w.dump(ordered_dict, file_obj, multiline_strings=True)
 
 if __name__ == '__main__':
     fire.Fire(Planner)
